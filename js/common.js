065bc3bde8629ef93f59625278a771bbefe4e206
sadlyk <sadlykings@gmail.com>
2017-10-06 15:28:17 +0300

    FIles

--- js/common.js
+++ js/common.js
@@ -1,5 +1,35 @@
 $(function() {
 
-	// Custom JS
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    width:90,
+    responsiveClass:true,
+    dots: false,
+    navText: ["<img src=\"img/arrow-left.svg\">","<img src=\"img/arrow-right.svg\">"],
+    responsive:{
+        320:{
+            items:1,
+            nav:false
+        },
+        480:{
+            items:2,
+            nav:true
+        },
+        800:{
+            items:3,
+            nav:true
+        },
+        1100:{
+            items:3,
+            nav:true
+        },
+        1200:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
 
 });
