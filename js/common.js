f17c403a98f33b98177ff65866144c3b13f9ad0c
sadlyk <sadlykings@gmail.com>
2018-02-16 14:37:58 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,21 +1,5 @@
-$(window).scroll(function(){
-      if ($(this).scrollTop() > 200) {
-          $('.fixed-menu').addClass('fix');
-      } else {
-          $('.fixed-menu').removeClass('fix');
-      }
-      if ( $(".fixed-menu").hasClass("fix") ) {
-      	$(".menu-icon-wrapper svg path").css(
-        {'stroke': '#48587F'}
-      );
-			} else {
-				$(".menu-icon-wrapper svg path").css(
-        {'stroke': '#fff'}
-      );
-			};
-			if ( $(".perspective").hasClass("modalview") ) {
-				$(".menu-icon-wrapper svg path").css(
-        {'stroke': '#48587F'}
-      );
-			};
+$(function() {
+
+	// Custom JS
+
 });
